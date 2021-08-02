1d82652f069d6a9e7689b680aeafee851a6a32d1
fish895623 <dan990429@gmail.com>
2021-08-02 18:19:02 +0900

    네이버 사이트 열기

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+const {app, BrowserWindow} = require('electron')
+
+app.on('ready', () => {
+    let win = new BrowserWindow({width: 800, height: 600})
+    win.loadURL('https://naver.com').then()
+})
+
+app.on('window-all-closed', () => {
+    app.quit()
+})
