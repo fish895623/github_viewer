1946cb2c522270f1601e857a56fe81eeb0328a3b
fish895623 <dan990429@gmail.com>
2021-08-02 22:45:29 +0900

    Set Data Path to show

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const {app, BrowserWindow} = require('electron');
+const userDataPath = app.getPath('userData');
+const path = require('path');
+const execPath = path.dirname(process.execPath);
+
+// 캐시 저장 장소 지정
+app.setPath('userData', execPath + '/' + 'data');
 
 app.on('ready', () => {
-    let win = new BrowserWindow({width: 800, height: 600});
-    win.loadURL('https://github.com/fish895623').then();
+  let win = new BrowserWindow({width: 800, height: 600});
+  win.loadURL('https://github.com/fish895623').then();
+  console.log(execPath);
 });
 
 app.on('window-all-closed', () => {
-    app.quit();
+  app.quit();
 });
