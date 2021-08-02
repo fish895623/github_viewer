3f781533b7bb77d6e089536ac558579bbce22632
fish895623 <dan990429@gmail.com>
2021-08-02 20:19:50 +0900

    내 깃허브 프로필 이동

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow} = require('electron');
 
 app.on('ready', () => {
-    let win = new BrowserWindow({width: 800, height: 600})
-    win.loadURL('https://naver.com').then()
-})
+    let win = new BrowserWindow({width: 800, height: 600});
+    win.loadURL('https://github.com/fish895623').then();
+});
 
 app.on('window-all-closed', () => {
-    app.quit()
-})
+    app.quit();
+});
