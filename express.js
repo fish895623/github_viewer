a0dc3d5e10f620b993d42bc4d45ae1ff803623ea
fish895623 <dan990429@gmail.com>
2021-08-02 20:25:18 +0900

    indent size 4 to 2

--- express.js
+++ express.js
@@ -3,13 +3,13 @@ let app = express();
 
 
 app.get(
-    '/test',
-    function (req, res) {
-        res.writeHead(200, {"Content-Type": "application/json;charset=utf-8"});
-        res.end('{"testcode":"200", "text":"Electorn Test~"}');
-    });
+  '/test',
+  function (req, res) {
+      res.writeHead(200, {"Content-Type": "application/json;charset=utf-8"});
+      res.end('{"response":"200", "text":"electron Test~"}');
+  });
 
 app.listen(3000,
-    function () {
-        console.log("출력중");
-    });
+  function () {
+      console.log("출력중");
+  });
